f13fcfb069a782bc364011c7bcacc313a7590f70
Sebastián <sebastianpozzi@gmail.com>
2022-06-08 18:03:29 -0300

    Completo todo

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,5 +1,7 @@
 // No cambies los nombres de las funciones.
 
+const { join } = require("@11ty/eleventy/src/TemplatePath");
+
 function deObjetoAmatriz(objeto) {
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
@@ -30,10 +32,11 @@ function numberOfCharacters(string) {
   };
 
   let caracteres = string.split("");
+
   for (const caracter of caracteres) {
     if (acumulado.hasOwnProperty(caracter)) {
-      acumulado.caracter + 1;
-    } else acumulado.caracter = 1;
+      acumulado[caracter] = acumulado[caracter] + 1;
+    } else acumulado[caracter] = 1;
   }
 
   return acumulado;
@@ -47,21 +50,29 @@ function capToFront(s) {
   //Escribe tu código aquí
   let characters = s.split("");
 
-  for (const character of characters) {
-    function retirarLetra() {
-      let indice = characters.indexOf(character);
-      if (indice > -1) {
-        characters.splice(indice, 1);
-      }
-    }
+  let mayusculas = [];
 
+  let minusculas = [];
+
+  let charactersOrdenados = [];
+
+  for (const character of characters) {
     if (character === character.toUpperCase()) {
-      retirarLetra();
-      characters.unshift(character);
+      mayusculas.push(character);
+    } else {
+      minusculas.push(character);
     }
   }
 
-  return characters;
+  for (const character of mayusculas) {
+    charactersOrdenados.push(character);
+  }
+
+  for (const character of minusculas) {
+    charactersOrdenados.push(character);
+  }
+
+  return charactersOrdenados.join("");
 }
 
 
@@ -71,6 +82,16 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  let frase = str.split(" ");
+
+  let fraseInvertida = [];
+
+  for (const palabra of frase) {
+    let palabraInvertida = palabra.split("").reverse().join("");
+    fraseInvertida.push(palabraInvertida);
+  }
+
+  return fraseInvertida.join(" ");
 }
 
 
@@ -79,6 +100,11 @@ function capicua(numero) {
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let numeroInvertido = numero.toString().split("").reverse().join("");
+
+  if (numero.toString() === numeroInvertido) {
+    return "Es capicua";
+  } else return "No es capicua";
 }
 
 
@@ -86,6 +112,17 @@ function deleteAbc(cadena) {
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  let conjunto = cadena.split("");
+
+  let cadenaFinal = [];
+
+  for (const letra of conjunto) {
+    if (letra !== "a" && letra !== "b" && letra !== "c") {
+      cadenaFinal.push(letra);
+    }
+  }
+
+  return cadenaFinal.join("");
 }
 
 
@@ -93,6 +130,17 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j].length > arr[j + 1].length) {
+        let suplente = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = suplente;
+      }
+    }
+  }
+
+  return arr;
 }
 
 
@@ -102,6 +150,15 @@ function buscoInterseccion(arreglo1, arreglo2) {
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  let interseccion = [];
+
+  for (const numero of arreglo1) {
+    if (arreglo2.includes(numero)) {
+      interseccion.push(numero);
+    }
+  }
+
+  return interseccion;
 }
 
 
